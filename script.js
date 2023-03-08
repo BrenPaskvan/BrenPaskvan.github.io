a6503efcffba6b365cecbe09fd7a6ad8aeba3d68
BrenPaskvan <126697161+BrenPaskvan@users.noreply.github.com>
2023-03-08 15:12:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.getElementById('Teléfono').onclick = function () {
+     document.getElementById("Teléfono").innerHTML = " 0341677665540"
+}
+
+document.getElementById('Email').onclick = function () {
+     document.getElementById("Email").innerHTML = "pskvnairbrenda@hotmail.com"
+}
+
+document.getElementById('Web').onclick = function () {
+     document.getElementById("Web").innerHTML = "www.estaesmidireccionweb.com.ar"
+}
+
+document.getElementById('Dirección').onclick = function () {
+     document.getElementById("Dirección").innerHTML = "Campana 2233. Argentina"
+}
